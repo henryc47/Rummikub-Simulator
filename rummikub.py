ebb3fee97ac86698b9cabf705d5044bf5a7b3418
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-01-10 00:45:27 +1100

    added in code to remove tiles from the bag

--- rummikub.py
+++ rummikub.py
@@ -20,13 +20,16 @@ class Tile:
         else:
             print(self.colour," ",self.number)
 
+#the bag stores the tiles which have not yet been selected by the player
 class Bag:
     __slots__ = ("tiles")
+    #fill the bag with tiles and shuffle them
     def __init__(self):
         self.tiles = self.create_tiles(colour_list,max_number,num_tile_copies,num_jokers)
         shuffle(self.tiles)
     
-    def create_tiles(self,colours : list[str],max_number : int,num_copies : int, num_jokers : int):
+    #create the tiles which fill the bag
+    def create_tiles(self,colours : list[str],max_number : int,num_copies : int, num_jokers : int) ->list[Tile]:
         tiles : list[Tile] = []
         #create regular tiles
         for colour in colours:
@@ -43,6 +46,20 @@ class Bag:
     def display_tiles(self):
         for tile in self.tiles:
             tile.display()
+    
+    #pick out a tile
+    def pick_tile(self) -> Tile:
+        if len(self.tiles)==0:
+            return None
+        else:
+            new_tile = self.tiles.pop()
+            return new_tile
 
 if __name__ == "__main__":
     game_bag = Bag()
+    game_bag.display_tiles()
+    print()
+    picked_tile = game_bag.pick_tile()
+    picked_tile.display()
+    print()
+    game_bag.display_tiles()
