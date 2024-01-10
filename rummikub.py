b80e0a2e281ef0d7a1e61faee8bd4e387a564a85
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-01-10 16:51:45 +1100

    added in methods to remove a specified tile from the hand, as well as make the display function for hands and bags indicate the number of tiles in the list

--- rummikub.py
+++ rummikub.py
@@ -16,11 +16,25 @@ class Tile:
         self.colour = colour
         self.number = number
 
+    #display a tiles contents
     def display(self):
         if self.is_joker:
             print("Joker")
         else:
             print(self.colour," ",self.number)
+
+    #does this tile match up with the requested tile
+    def match_tile(self,is_joker : bool ,colour : str, number : int) -> bool:
+        if is_joker:
+            if self.is_joker:
+                return True
+            else:
+                return False
+        else:
+            if (self.colour==colour) and (self.number==number):
+                return True
+            else:
+                return False
 
 #the bag stores the tiles which have not yet been selected by the player
 class Bag:
@@ -45,11 +59,14 @@ class Bag:
             tiles.append(joker)
         return tiles
     
+    #display all the tiles in the bag
     def display_tiles(self):
         for tile in self.tiles:
             tile.display()
+        print("num tiles in bag = ",len(self.tiles))
+        print()
     
-    #pick out a tile
+    #pick out a tile and remove it from the bag
     def pick_tile(self) -> Tile:
         if len(self.tiles)==0:
             return None
@@ -64,6 +81,7 @@ class Hand:
         self.bag = bag#bag we are drawing cards from
         self.tiles = self.get_starting_hand(starting_hand_size)
 
+    #get a starting hand of the specificed size from the bag
     def get_starting_hand(self, starting_hand_size : int):
         tiles : list[Tile] = []
         for i in range(starting_hand_size):
@@ -71,13 +89,27 @@ class Hand:
             tiles.append(new_tile)
         return tiles
 
+    #pick an individual tile from the bag and add it to the hand
     def pick_tile_from_bag(self):
         new_tile = self.bag.pick_tile()
         self.tiles.append(new_tile)
+    
+    #remove a tile from the hand
+    def remove_tile_from_hand(self,tile_is_joker : bool,tile_colour : str,tile_number : int) -> bool:
+        found_tile = False
+        for i,tile in enumerate(self.tiles):
+            tile_match = tile.match_tile(tile_is_joker,tile_colour,tile_number)
+            if tile_match:
+                found_tile = True
+                self.tiles.pop(i)
+                break
+        return found_tile
 
     def display_hand(self):
         for tile in self.tiles:
             tile.display()
+        print("num tiles in hand = ",len(self.tiles))
+        print()
 
 #The player class represents a player agent, includes a hand object which stores the games hand. Derived classes encode strategies
 class Player:
@@ -93,13 +125,12 @@ if __name__ == "__main__":
     hand = Hand(game_bag,starting_hand_size)
     print("hand")
     hand.display_hand()
-    print("in hand = ",len(hand.tiles))
-    print()
-    hand.pick_tile_from_bag()
-    print("hand after picking")
+    found_tile = hand.remove_tile_from_hand(True,"",-1)
+    print("found tile = ",found_tile)
+    print("hand after picking joker ")
     hand.display_hand()
-    print("in hand = ",len(hand.tiles))
-    print()
-    print("bag")
-    game_bag.display_tiles()
-    print("in bag = ",len(game_bag.tiles))
+    found_tile = hand.remove_tile_from_hand(False,"Red",8)
+    print("found tile = ",found_tile)
+    print("hand after picking Red 8 ")
+    hand.display_hand()
+    
